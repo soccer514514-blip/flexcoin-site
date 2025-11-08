7a98f8d22d923fed4ab9be3bb36218d7b5c799bd
soccer514514-blip <soccer514514@gmail.com>
2025-11-08 14:35:18 +0900

    Add files via upload

---
+++ src/snippets/paths.ts
@@ -0,0 +1,18 @@
+export const HERO_IMAGES = [
+  "/hero/1.jpg",
+  "/hero/2.jpg",
+  "/hero/3.jpg",
+  "/hero/4.jpg",
+  "/hero/5.jpg",
+  "/hero/6.jpg",
+  "/hero/7.jpg",
+];
+export const MAIN_HERO = "/hero/main-hero.jpg";
+export const NFT_PREVIEWS = [
+  "/nft-preview/1.jpg",
+  "/nft-preview/2.jpg",
+  "/nft-preview/3.jpg",
+  "/nft-preview/4.jpg",
+  "/nft-preview/5.jpg",
+  "/nft-preview/6.jpg",
+];
