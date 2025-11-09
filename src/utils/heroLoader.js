15e0dec372a04c3acd304ed608c53ecdc664c139
soccer514514-blip <soccer514514@gmail.com>
2025-11-10 00:33:08 +0900

    Delete src/utils/heroLoader.js

--- src/utils/heroLoader.js
+++
@@ -1,5 +0,0 @@
-
-export async function resolveHeroMain(){
-  const test=(u)=>new Promise(res=>{ const i=new Image(); i.onload=()=>res(true); i.onerror=()=>res(false); i.src=u; });
-  const m="/hero/main.jpg"; return (await test(m))?m:"/hero/1.jpg";
-}
